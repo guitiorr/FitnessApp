9ef870885dc96b28e1d702a44855747113320b66
justin lovell <justinlvell18@gmail.com>
2024-06-06 15:08:21 +0700

    Editting style

--- Layouts/Header.Master.designer.cs
+++ Layouts/Header.Master.designer.cs
@@ -51,15 +51,6 @@ namespace FitnessApp.Layouts
         protected global::System.Web.UI.WebControls.Button RegisterButton;
 
         /// <summary>
-        /// LogOutButton control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button LogOutButton;
-
-        /// <summary>
         /// HomeButton control.
         /// </summary>
         /// <remarks>
@@ -103,6 +94,15 @@ namespace FitnessApp.Layouts
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button StudentButton;
+
+        /// <summary>
+        /// LogOutButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button LogOutButton;
 
         /// <summary>
         /// ProfileButton control.
